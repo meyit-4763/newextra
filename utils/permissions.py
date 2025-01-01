000b635c5311a59c209674007e8d4bfce7c85861
meyit-4763 <sumeyyeomeryesilyurt@gmail.com>
2025-01-01 23:46:33 +0300

    Update permissions.py

--- utils/permissions.py
+++ utils/permissions.py
@@ -4,8 +4,8 @@ from traceback import format_exc as err
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from DnsXMusic import app
+from DnsXMusic.misc import SUDOERS
 
 
 async def member_permissions(chat_id: int, user_id: int):
