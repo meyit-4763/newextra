6f760a43703808b34b2628a2945ae921ef121417
meyit-4763 <sumeyyeomeryesilyurt@gmail.com>
2025-01-02 00:07:44 +0300

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -3,7 +3,7 @@ import os
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from TheApi import api
-from YukkiMusic import app
+from DnsXMusic import app
 
 
 @app.on_message(filters.command(["tgm", "tgt", "telegraph", "tl"]))
